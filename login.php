3a64c9f193a0c202a8819fb1cb70dc815e891032
Genta Arimbawa <133459038+Mastayxz@users.noreply.github.com>
2023-05-13 19:54:58 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,48 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login PHP </title>
+</head>
+<body>
+<h2>Login User </h2>
+    <br>
+    <!-- cek notifikasi -->
+     <?php 
+     if(isset($_GET['pesan'])){
+        if($_GET['pesan']== "gagal"){
+            echo "gagal";
+        }else if($_GET['pesan']=="login"){
+            echo "anda telah berhasil login";
+            header("location: menu.php");
+            exit();
+        }
+     }
+     ?>
+     
+    <br>
+    <br>
+    <form action="ceklogin.php" method = "post">
+            <table>
+                <tr>
+                    <td>username</td>
+                    <td>:</td>
+                    <td><input type="text" name="username" placeholder="masukan username" id=""></td>
+                </tr>
+                <tr>
+                    <td>password</td>
+                    <td>:</td>
+                    <td><input type="password" name="password" placeholder="********" id=""></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td><input type="submit" value="LOGIN"></td>
+                </tr>
+                
+
+            </table>
+    </form>
+</body>
+</html>
